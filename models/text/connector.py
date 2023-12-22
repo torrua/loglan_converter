45c0fde68753f84d3942653baa87a4613a04d1c2
torrua <torrua@users.noreply.github.com>
2023-12-22 08:13:19 +0700

    Draft connector

---
+++ models/text/connector.py
@@ -0,0 +1,26 @@
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+
+import os
+from sqlalchemy import Engine, create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+
+class TextConnector:
+
+    def __init__(self, path: str):
+        if self.is_path(path):
+            self.engine = self.get_engine(path)
+
+    @classmethod
+    def get_engine(cls, path: str) -> Engine:
+        return create_engine(path)
+
+    def session(self) -> Session:
+        return sessionmaker(bind=self.engine, future=True)()
+
+    @staticmethod
+    def is_path(path: str) -> bool:
+        if os.path.exists(path):
+            return True
+        else:
+            raise FileNotFoundError("Directory not found")
