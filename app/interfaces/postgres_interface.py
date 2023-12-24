f42e812c9c1b96889d764117e41d96dad1c7fad7
torrua <torrua@users.noreply.github.com>
2023-12-24 15:47:50 +0700

    Update Interfaces with Storage

--- app/interfaces/postgres_interface.py
+++ app/interfaces/postgres_interface.py
@@ -1,10 +1,12 @@
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
 from app.interfaces.database_interface import DatabaseInterface
+from app.models.storage.storage import Storage
 
 
 class PostgresInterface(DatabaseInterface):
 
-    def export_data(self):
+    def export_data(self) -> Storage:
         pass
 
-    def import_data(self, data):
+    def import_data(self, data: Storage):
         pass
