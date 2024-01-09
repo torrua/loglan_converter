96d1b0e5cd4e938fbf2ca0582c912742315761f5
torrua <torrua@users.noreply.github.com>
2024-01-09 07:36:36 +0700

    Update TableContainer methods

--- app/models/storage/table_container.py
+++ app/models/storage/table_container.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
 from __future__ import annotations
 
-from typing import Any, Iterable, Sized
+from typing import Any, Iterable, Sized, Type, get_args
 
 from app.models.storage.properties import TableProperties
 from logger import log
@@ -21,8 +21,9 @@ class TableContainer(list):
         (
             self.order,
             self.name,
-            self.pattern,
+            self._pattern,
         ) = table_properties
+        self.pattern = [prepared_types(types) for types in self._pattern]
 
     def __repr__(self):
         return f"{self.name}{self.__class__.__name__}({len(self)})"
@@ -96,6 +97,14 @@ class TableContainer(list):
             raise ValueError("Item is not suitable for this collection.")
         super().append(item_to_append)
 
+    def append_directly(self, item: Iterable[Any]):
+        """
+        Appends an item to the collection without conversion.
+        Parameters:
+            item (list[Any]): A list of elements to be appended.
+        """
+        super().append(item)
+
     def extend(self, iterable: Iterable[Iterable[Any]]):
         """
         Extends the collection by appending elements from the iterable.
@@ -104,6 +113,14 @@ class TableContainer(list):
         """
         for item in iterable:
             self.append(item)
+
+    def extend_directly(self, iterable: Iterable[Iterable[Any]]):
+        """
+        Extends the collection without conversion.
+        Parameters:
+            iterable: An iterable of list items to append to the list.
+        """
+        super().extend(iterable)
 
     def insert(self, index: int, item: Iterable[Any]):
         """
@@ -119,6 +136,15 @@ class TableContainer(list):
             super().insert(index, item)
         else:
             raise ValueError("Item is not suitable for this collection.")
+
+    def insert_directly(self, index: int, item: Iterable[Any]):
+        """
+        Inserts an item at a specified index. Overrides the parent class insert method.
+        Parameters:
+            index (int): The index at which the item should be inserted.
+            item: The item to insert into the collection.
+        """
+        super().insert(index, item)
 
     def __setitem__(self, index: int, item: Iterable[Any]):
         """
@@ -137,8 +163,7 @@ class TableContainer(list):
         else:
             raise ValueError("Item is not suitable for this collection.")
 
-    @staticmethod
-    def convert_item_elements(item: Iterable[Any]) -> tuple[Any, ...]:
+    def convert_item_elements(self, item: Iterable[Any]) -> list[Any]:
         """
         Converts elements of a list to their respective data types.
 
@@ -154,29 +179,8 @@ class TableContainer(list):
             list[Any]: A list with elements converted to their proper
                 data types.
         """
-
-        def convert_element(x: str) -> Any:
-            """
-            Converts a string to an integer, boolean, None, or keeps it as a string.
-            Parameters:
-                x (str): The string to convert.
-            Returns:
-                int: If 'x' is a digit.
-                bool: If 'x' is 'true'/'True' or 'false'/'False'.
-                None: If 'x' is an empty string.
-                str: If 'x' does not match any of the above conditions.
-            """
-            if x.isdigit():
-                return int(x)
-            if x.lower() == "true":
-                return True
-            if x.lower() == "false":
-                return False
-            if x == "":
-                return None
-            return x
-
-        return tuple(convert_element(x) for x in item)
+        check_pairs = zip(item, self.pattern)
+        return [convert_element(value, types) for value, types in check_pairs]
 
     def is_proper_pattern(self, item: Iterable[Any]) -> bool:
         """
@@ -193,26 +197,7 @@ class TableContainer(list):
             bool: True if each element in the list matches the expected
                 data type(s) from the pattern; False otherwise.
         """
-
-        for types, value in zip(self.pattern, item):
-            prepared_types = self.prepared_types(types)
-            if not isinstance(value, prepared_types):
-                log.debug(
-                    f"{False} for item {item} in '{value}' "
-                    f"with type '{type(value).__name__}': {prepared_types}"
-                )
-                return False
-        log.debug("Is proper types for all elements: True")
-        return True
-
-    @staticmethod
-    def prepared_types(expected_types):
-        if not isinstance(expected_types, tuple):
-            expected_types = (expected_types,)
-        expected_types = tuple(
-            type(None) if t is None else t for t in expected_types
-        )
-        return expected_types
+        return check_proper_pattern(item, self.pattern)
 
     @classmethod
     def generate_containers(
@@ -229,3 +214,76 @@ class TableContainer(list):
                 from the provided TableProperties data.
         """
         return tuple(cls(data) for data in table_properties_collection)
+
+
+def check_proper_pattern(item: Iterable[Any], pattern: list) -> bool:
+    """
+    Validates if each element in the input list matches a pattern
+    of expected types. The pattern is defined by the 'self.pattern'
+    attribute which contains types or tuples of types. None is
+    considered a valid type and is replaced by 'type(None)'.
+
+    Parameters:
+        item (Iterable[Any]): The list of elements to validate against the
+            pattern.
+        pattern (list): The expected types for each element in the list.
+    Returns:
+        bool: True if each element in the list matches the expected
+            data type(s) from the pattern; False otherwise.
+    """
+
+    for types, value in zip(pattern, item):
+        if not isinstance(value, types):
+            log.debug(
+                "False for item %s in '%s' with type '%s': %s",
+                item,
+                value,
+                type(value).__name__,
+                *types,
+            )
+            return False
+    log.debug("Is proper types for all elements: True")
+    return True
+
+
+def prepared_types(actual_types: Type | tuple[Type, ...]) -> tuple[Type, ...]:
+    """
+    Prepares the expected types for a function.
+
+    Parameters:
+        actual_types (Union[type, Tuple[type]]): The expected types for the function.
+            If a single type is provided, it will be converted to a tuple.
+            If None is provided, the type will be converted to type(None).
+    Returns:
+        tuple[Type]: The prepared expected types.
+    """
+    args = get_args(actual_types)
+    expected_types = args or (actual_types,)
+    expected_types = tuple(type(None) if t is None else t for t in expected_types)
+    return expected_types
+
+
+def convert_element(value, types) -> Any:
+    """
+    Converts the given `value` to the appropriate data type
+    based on the provided `types`.
+
+    Parameters:
+        value: The value to be converted.
+        types: A list of data types to check against.
+
+    Returns:
+        The converted value if a matching data type is found.
+        None if the `value` is empty or None.
+        The original `value` if no matching data type is found.
+    """
+
+    if not value and type(None) in types:
+        return None
+    if value.isdigit() and int in types:
+        return int(value)
+    if (value.lower() in ["true", "1"]) and (bool in types):
+        return True
+    if (value.lower() in ["false", "0"]) and (bool in types):
+        return False
+    return value
