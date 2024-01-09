df245df36cb1cb3b7e03d9e371347b2cb4b05fe5
torrua <torrua@users.noreply.github.com>
2024-01-09 07:39:57 +0700

    Update DatabaseInterface docs and attributes

--- app/interfaces/database_interface.py
+++ app/interfaces/database_interface.py
@@ -1,14 +1,28 @@
-# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+# pylint: disable=missing-module-docstring
 from abc import ABC, abstractmethod
 from app.models.storage.storage import Storage
 
 
 class DatabaseInterface(ABC):
+    """
+    An abstract class representing a database interface.
+    """
+    SEPARATOR = "@"
 
     @abstractmethod
     def export_data(self) -> Storage:
-        ...
+        """
+        Exports data from the database to a Storage object.
+
+        Returns:
+            Storage: A Storage object containing the exported data.
+        """
 
     @abstractmethod
     def import_data(self, data: Storage):
-        ...
+        """
+        Imports a Storage object to the database.
+
+        Parameters:
+            data (Storage): The Storage object to import.
+        """
