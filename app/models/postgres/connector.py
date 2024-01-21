a215c4d2efe88b9f97cb97cec0f183360e8f7255
torrua <torrua@users.noreply.github.com>
2024-01-21 21:52:13 +0700

    Update PostgresDatabaseConnector

--- app/models/postgres/connector.py
+++ app/models/postgres/connector.py
@@ -2,15 +2,65 @@
 
 import re
 
+from loglan_core import (
+    Author,
+    Event as BaseEvent,
+    Type,
+    Word as BaseWord,
+    WordSpell,
+    Definition,
+    Setting as BaseSetting,
+    Syllable,
+)
+from loglan_core.base import BaseModel
+from app.models.storage.properties import ClassName
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, Session
+from datetime import datetime
+
+
+class Event(BaseEvent):
+    DATE_FORMAT = "%m/%d/%Y"
+
+    def __init__(self, *args, **kwargs):
+        date_index = 2
+        args = list(args)
+        if args and isinstance(args[date_index], str):
+            args[date_index] = datetime.strptime(
+                args[date_index], self.DATE_FORMAT
+            )
+        super().__init__(*args, **kwargs)
+
+
+class Setting(BaseSetting):
+    DATE_FORMAT = "%d.%m.%Y %H:%M:%S"
+
+    def __init__(self, *args, **kwargs):
+        date_index = 0
+        args = list(args)
+        if args and isinstance(args[date_index], str):
+            args[date_index] = datetime.strptime(
+                args[date_index], self.DATE_FORMAT
+            )
+        super().__init__(*args, **kwargs)
 
 
 class PostgresDatabaseConnector:
+    TABLES_ORDER = {
+        ClassName.authors: Author,
+        ClassName.events: Event,
+        ClassName.types: Type,
+        ClassName.words: BaseWord,
+        ClassName.word_spells: WordSpell,
+        ClassName.definitions: Definition,
+        ClassName.settings: Setting,
+        ClassName.syllables: Syllable,
+    }
+
     def __init__(self, path: str):
         if self.is_path(path):
             self.path = path
-            self.engine = self.get_engine(self.path)
+            self.engine: Engine = self.get_engine(self.path)
 
     @classmethod
     def get_engine(cls, path: str) -> Engine:
@@ -22,9 +72,15 @@ class PostgresDatabaseConnector:
     @staticmethod
     def is_path(path: str) -> bool:
         if not path:
-            raise ValueError("No Postgresql URI provided. Please check your environment.")
+            raise ValueError(
+                "No Postgresql URI provided. Please check your environment."
+            )
 
         pattern = r"^postgres(?:ql)?://"
         if re.match(pattern, path) is None:
             raise ValueError(f"Invalid Postgresql URI:\n\t{path}")
         return True
+
+    def recreate_tables(self):
+        BaseModel.metadata.drop_all(bind=self.engine)
+        BaseModel.metadata.create_all(bind=self.engine)
