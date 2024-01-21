86b7db629423434e88742646a78a5afef5afa131
torrua <torrua@users.noreply.github.com>
2024-01-21 21:52:45 +0700

    Update example

--- app/models/text/example.py
+++ app/models/text/example.py
@@ -3,10 +3,12 @@
 import os
 
 from app.models.text.connector import TextConnector
+from app.models.storage.properties import ClassName
 
 if __name__ == "__main__":
     dir_path = os.getenv("TXT_DATABASE_URL")
     tc = TextConnector(dir_path)
     print(tc)
     print(tc.files_paths)
-    print(tc.file_by_name("Author"))
+    print(tc.path_by_name(ClassName.authors))
+    print(tc.content_by_name(ClassName.authors))
