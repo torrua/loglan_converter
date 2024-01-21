9de1c6691289f7e66a2c914d7b204a9795b1739f
torrua <torrua@users.noreply.github.com>
2024-01-21 21:48:43 +0700

    Complete PostgresInterface

--- app/interfaces/postgres_interface.py
+++ app/interfaces/postgres_interface.py
@@ -1,7 +1,27 @@
-# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+# pylint: disable=missing-module-docstring, missing-class-docstring
+
+from loglan_core import Author, Type, Word, Key, Definition, WordSelector
+from loglan_core.addons.definition_selector import DefinitionSelector
+from loglan_core.addons.exporter import Exporter
+from loglan_core.addons.word_linker import WordLinker
+from sqlalchemy import func, select
+
 from app.interfaces.database_interface import DatabaseInterface
 from app.models.postgres.connector import PostgresDatabaseConnector
+from app.models.postgres.functions import (
+    extract_keys,
+    get_grammar,
+    get_word_names,
+    get_word_data,
+    get_unique_keys_strings,
+    get_source_data_by_index,
+    generate_complex_children,
+    generate_djifoa_children,
+    generate_authors_data,
+)
+from app.models.storage.properties import ClassName
 from app.models.storage.storage import Storage
+from logger import log, logging_time
 
 
 class PostgresInterface(DatabaseInterface):
@@ -11,8 +31,205 @@ class PostgresInterface(DatabaseInterface):
         """
         self.connector = connector
 
+    @logging_time
     def export_data(self) -> Storage:
-        pass
+        s = Storage()
+        with self.connector.session() as session:
+            for container, class_ in zip(
+                s.containers, self.connector.TABLES_ORDER.values()
+            ):
+                objects = session.query(class_).all()
+                log.info("Exporting %s", class_.__name__)
 
-    def import_data(self, data: Storage):
-        pass
+                for obj in objects:
+                    exp = Exporter.export(obj, self.SEPARATOR)
+                    exported_object = exp.split(self.SEPARATOR)
+                    container.append(exported_object)
+
+                log.info("Exported %s %s items\n", len(container), class_.__name__)
+        # TODO Implement direct exporting
+        return s
+
+    @logging_time
+    def import_data(self, data: Storage) -> None:
+        self.import_simple_classes(data)
+        self.import_words(data)
+        self.import_definitions(data, "en")
+        self.add_keys("en")
+        self.link_keys()
+        self.link_authors(data)
+        self.link_complexes(data)
+        self.link_affixes(data)
+
+    def import_simple_classes(self, data):
+        for class_name in [
+            ClassName.authors,
+            ClassName.events,
+            ClassName.types,
+            ClassName.settings,
+            ClassName.syllables,
+        ]:
+            with self.connector.session() as session:
+                class_ = self.connector.TABLES_ORDER.get(class_name)
+                log.info("Importing %s", class_.__name__)
+                container = data.container_by_name(class_name)
+                objects = [class_(*item) for item in container]
+                session.bulk_save_objects(objects)
+                session.commit()
+                log.info("Imported %s %s items\n", len(container), class_.__name__)
+
+    def import_words(self, data):
+        class_ = self.connector.TABLES_ORDER.get(ClassName.words)
+        log.info("Importing %s", class_.__name__)
+
+        words = data.container_by_name(ClassName.words)
+        spell = data.container_by_name(ClassName.word_spells)
+
+        with self.connector.session() as session:
+            types_data = session.query(Type.type, Type.id).all()
+            types = dict((item.type, item.id) for item in types_data)
+
+        data = get_word_data(words, types)
+        names = get_word_names(spell)
+
+        words = [
+            Word(
+                **{
+                    "name": names.get(index).get("name"),
+                    "event_start_id": names.get(index).get("event_start_id"),
+                    "event_end_id": names.get(index).get("event_end_id"),
+                    "id_old": data.get(index[1]).get("id_old"),
+                    "origin": data.get(index[1]).get("origin"),
+                    "origin_x": data.get(index[1]).get("origin_x"),
+                    "type_id": data.get(index[1]).get("type_id"),
+                    "match": data.get(index[1]).get("match"),
+                    "rank": data.get(index[1]).get("rank"),
+                    "year": data.get(index[1]).get("year"),
+                    "notes": data.get(index[1]).get("notes"),
+                    "tid_old": data.get(index[1]).get("tid_old"),
+                }
+            )
+            for index in names.keys()
+        ]
+
+        words.sort(key=lambda x: x.name)
+
+        with self.connector.session() as session:
+            session.bulk_save_objects(words)
+            session.commit()
+
+        log.info("Imported %s %s items\n", len(words), class_.__name__)
+
+    def import_definitions(self, data, language: str):
+        definitions = data.container_by_name(ClassName.definitions)
+
+        all_definitions = []
+        with self.connector.session() as session:
+            for item in definitions:
+                words = (
+                    session.execute(WordSelector().filter(Word.id_old == int(item[0])))
+                    .scalars()
+                    .all()
+                )
+                for word in words:
+                    all_definitions.append(
+                        Definition(
+                            **{
+                                "word_id": word.id,
+                                "position": int(item[1]),
+                                "usage": item[2],
+                                "slots": get_grammar(item[3])["slots"],
+                                "grammar_code": get_grammar(item[3])["code"],
+                                "body": item[4],
+                                "language": language,
+                                "case_tags": item[6],
+                            }
+                        )
+                    )
+            session.bulk_save_objects(all_definitions)
+            session.commit()
+
+    @logging_time
+    def add_keys(self, language: str):
+        log.info(
+            "Importing %s", Key.__name__
+        )  # TODO Rewrite for getting language from definitions
+
+        with self.connector.session() as session:
+            bodies = session.query(func.string_agg(Definition.body, "@")).scalar()
+            keys = extract_keys(bodies, language)
+            session.bulk_save_objects(keys)
+            session.commit()
+        log.info("Imported %s %s items\n", len(keys), Key.__name__)
+
+    @logging_time
+    def link_keys(self):
+        with self.connector.session() as session:
+            for definition in DefinitionSelector().all(session):
+                keys_str = get_unique_keys_strings(definition.body)
+                keys_obj = (
+                    session.execute(
+                        select(Key)
+                        .filter(Key.word.in_(keys_str))
+                        .filter(Key.language == definition.language)
+                    )
+                    .scalars()
+                    .all()
+                )
+                definition.keys_query.extend(keys_obj)
+            session.commit()
+
+    @logging_time
+    def link_authors(self, data: Storage):
+        authors_data = generate_authors_data(data)
+
+        with self.connector.session() as session:
+            all_authors = session.execute(select(Author)).scalars().all()
+            author_by_abbr = dict(
+                (author.abbreviation, author) for author in all_authors
+            )
+
+            for word in WordSelector().all(session):
+                authors_abbrs = authors_data[word.id_old]
+                authors = [author_by_abbr[abbr] for abbr in authors_abbrs]
+                WordLinker.add_authors(word, authors)
+
+            session.commit()
+
+    @logging_time
+    def link_complexes(self, data: Storage):
+        index_used_in = 10
+        words = get_source_data_by_index(data, index_used_in)
+        self.link_words(words, generate_complex_children)
+
+    @logging_time
+    def link_affixes(self, data: Storage):
+        index_affixes = 3
+        words = get_source_data_by_index(data, index_affixes)
+        self.link_words(words, generate_djifoa_children)
+
+    def link_words(self, words, generate_children_func):
+        with self.connector.session() as session:
+            for w in words:
+                stmt = WordSelector().filter(Word.id_old == int(w[0]))
+                parents = session.execute(stmt).scalars().all()
+
+                for parent in parents:
+                    children = generate_children_func(w, session)
+                    WordLinker.add_children(parent, children)
+            session.commit()
+
+
+if __name__ == "__main__":
+    import os
+
+    URI = os.environ.get("LOD_DATABASE_URL")
+    pdc = PostgresDatabaseConnector(URI)
+    pi = PostgresInterface(pdc)
+    storage = pi.export_data()
+
+    URI_IMPORT = os.environ.get("LOD_DATABASE_IMPORT")
+    pdc_import = PostgresDatabaseConnector(URI_IMPORT)
+    pdc_import.recreate_tables()
+    pi_import = PostgresInterface(pdc_import)
+    pi_import.import_data(storage)
---
+++ app/models/postgres/functions.py
@@ -0,0 +1,128 @@
+import re
+from datetime import datetime
+
+from loglan_core import Key, Word, WordSelector
+
+from app.models.storage.properties import ClassName
+from app.models.storage.storage import Storage
+
+
+def extract_keys(bodies: str, language: str) -> list[Key]:
+    keys = get_unique_keys_strings(bodies)
+    return [Key(**{"word": key, "language": language}) for key in keys]
+
+
+def get_unique_keys_strings(text: str) -> list[str]:
+    key_pattern = r"(?<=\«)(.+?)(?=\»)"
+    all_keys = re.findall(key_pattern, text)
+    return sorted(set(all_keys))
+
+
+def get_grammar(str_grammar: str) -> dict:
+    slots = re.search(r"\d", str_grammar)
+    code = re.search(r"\D+", str_grammar)
+
+    return {
+        "slots": int(slots.group(0)) if slots else None,
+        "code": code.group(0) if code else "",
+    }
+
+
+def get_year(str_date: str) -> dict:
+    date_year = str_date.split(" ", 1)
+    return {
+        "year": datetime.strptime(date_year[0], "%Y").date(),
+        "notes": date_year[1] if len(date_year) > 1 else None,
+    }
+
+
+def get_rank(str_rank: str) -> dict:
+    if not str_rank:
+        return {"rank": None, "notes": None}
+
+    rank_data = str_rank.split(" ", 1)
+    return {
+        "rank": rank_data[0],
+        "notes": rank_data[1] if len(rank_data) > 1 else None,
+    }
+
+
+def get_author(str_author) -> dict:
+    author_data = str_author.split(" ", 1)
+    return {
+        "author": author_data[0],
+        "notes": author_data[1] if len(author_data) > 1 else None,
+    }
+
+
+def get_notes(item: list) -> dict | None:
+    str_notes = {
+        "author": get_author(item[5]).get("notes"),
+        "year": get_year(item[6]).get("notes"),
+        "rank": get_rank(item[7]).get("notes"),
+    }
+    return {k: v for k, v in str_notes.items() if v} or None
+
+
+def get_word_names(spell):
+    dict_of_word_names = {
+        (index, int(item[0])): {
+            "name": item[1],
+            "id_old": int(item[0]),
+            "event_start_id": int(item[4]),
+            "event_end_id": int(item[5]) if int(item[5]) < 9999 else None,
+        }
+        for index, item in enumerate(spell)
+    }
+    return dict_of_word_names
+
+
+def get_word_data(words, types):
+    dict_of_word_data = {
+        item[0]: {
+            "authors": get_author(item[5]).get("author"),
+            "type_id": types.get(item[1]),
+            "origin": item[8],
+            "origin_x": item[9],
+            "id_old": int(item[0]),
+            "match": item[4],
+            "rank": get_rank(item[7]).get("rank"),
+            "year": get_year(item[6]).get("year"),
+            "tid_old": int(item[11]) if item[11] else None,
+            "notes": get_notes(item),
+        }
+        for item in words
+    }
+    return dict_of_word_data
+
+
+def get_elements_from_str(set_as_str: str, separator: str) -> list:
+    return [element.strip() for element in set_as_str.split(separator)]
+
+
+def get_source_data_by_index(data: Storage, index: int) -> list:
+    words = [w for w in data.container_by_name(ClassName.words) if w[index]]
+    return words
+
+
+def generate_complex_children(w: list, session):
+    child_names = get_elements_from_str(w[10], separator=" | ")
+    stmt = WordSelector().filter(Word.name.in_(child_names))
+    children = list(session.execute(stmt).scalars().all())
+    return children
+
+
+def generate_djifoa_children(w: list, session):
+    djifoa = get_elements_from_str(w[3], separator=" ")
+    djifoa_with_hyphen = [f"{df}-" for df in djifoa]
+    all_djifoa = djifoa + djifoa_with_hyphen
+    stmt = WordSelector().by_type(type_="Afx").filter(Word.name.in_(all_djifoa))
+    children = list(session.execute(stmt).scalars().all())
+    return children
+
+
+def generate_authors_data(data: Storage) -> dict:
+    return {
+        int(w[0]): w[5].split(" ", 1)[0].split("/")
+        for w in data.container_by_name(ClassName.words)
+    }
