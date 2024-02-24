944d94f6331a36b3e3bfd99bbd02126f3a6cf523
torrua <torrua@users.noreply.github.com>
2024-02-24 14:14:00 +0700

    Move DB preparation to Connector

--- app/models/access/connector.py
+++ app/models/access/connector.py
@@ -5,8 +5,9 @@ from urllib.parse import quote_plus
 
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session, sessionmaker
+
 from app.connector import DatabaseConnector
-from app.properties import ClassName
+from app.models.access.functions import db_compress_file, db_clear_content
 from app.models.access.model import (
     AccessAuthor,
     AccessEvent,
@@ -17,6 +18,8 @@ from app.models.access.model import (
     AccessWordSpell,
     AccessDefinition,
 )
+from app.properties import ClassName
+from logger import log
 
 
 class AccessDatabaseConnector(DatabaseConnector):
@@ -24,10 +27,17 @@ class AccessDatabaseConnector(DatabaseConnector):
         r"DRIVER={{Microsoft Access Driver (*.mdb, *.accdb)}};DBQ={};ExtendedAnsiSQL=1;"
     )
 
-    def __init__(self, path: str):
-        if self.is_path(path):
-            self.path = path
-            self.engine = self.get_engine(self.path)
+    def __init__(self, path: str, importing: bool = False):
+        self.is_path(path)
+        self.path = path
+        self.engine = self.get_engine(self.path)
+
+        if importing:
+            log.info("Clearing database")
+            db_clear_content(self)
+
+            log.info("Compressing database")
+            db_compress_file(self)
 
     @classmethod
     def get_engine(cls, path: str) -> Engine:
@@ -55,7 +65,7 @@ class AccessDatabaseConnector(DatabaseConnector):
         return sessionmaker(bind=self.engine)()
 
     @staticmethod
-    def is_path(path: str) -> bool:
+    def is_path(path: str):
         if not path:
             raise ValueError(
                 "No Microsoft Access URI provided. Please check your environment."
@@ -64,4 +74,3 @@ class AccessDatabaseConnector(DatabaseConnector):
         pattern = r"^.*\.mdb|accdb$"
         if re.match(pattern, path) is None:
             raise ValueError(f"Invalid Microsoft Access URI:\n\t{path}")
-        return True
