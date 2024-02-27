815d70b58e0251eeac15448c78309cb4b7472fc7
torrua <torrua@users.noreply.github.com>
2024-02-27 08:55:54 +0700

    Add pylint rules

--- app/models/common.py
+++ app/models/common.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+# pylint: disable=too-few-public-methods
 
 class CommonEvent:
     DATE_FORMAT = "%m/%d/%Y"
